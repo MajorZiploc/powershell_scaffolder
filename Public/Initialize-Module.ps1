c977dece56a31f201c22ae8f27f8d281f8e6e5ed
Manyu Lakhotia <lakemonyew@gmail.com>
2021-01-25 10:01:34 -0600

    Fixes indention

--- Public/Initialize-Module.ps1
+++ Public/Initialize-Module.ps1
@@ -177,7 +177,7 @@ $ModuleName
           $content = Get-Content -Path $_
           Set-Content -Path $_.Fullname -Value $content -Encoding UTF8 -PassThru -Force
         }
-}
+      }
 
     }
     catch {
