eefb0dad50a4f57aa4282079b6b4befa10b37388
Manyu Lakhotia <lakemonyew@gmail.com>
2021-02-03 11:17:25 -0600

    Fixes Get-ErrorDetails fn

--- Public/Initialize-Module.ps1
+++ Public/Initialize-Module.ps1
@@ -192,12 +192,12 @@ function Get-ErrorDetails {
   )
 
   return @{
-    ScriptStackTrace = $error.ScriptStackTrace
-    StackTrace = $error.Exception.StackTrace
-    Message = $error.Exception.Message
-    FullyQualifiedErrorId = $error.FullyQualifiedErrorId
-    TargetObject = $error.TargetObject
-    ErrorDetails = $error.ErrorDetails
+    ScriptStackTrace = `$error.ScriptStackTrace
+    StackTrace = `$error.Exception.StackTrace
+    Message = `$error.Exception.Message
+    FullyQualifiedErrorId = `$error.FullyQualifiedErrorId
+    TargetObject = `$error.TargetObject
+    ErrorDetails = `$error.ErrorDetails
   }
 }
 
