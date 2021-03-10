fbd4daa960ff283ffd8000066d47bbe8772280c1
Manyu Lakhotia <abhimanyu.lakhotia@acadian.com>
2021-03-10 08:41:29 -0600

    Adds Initialize-Script fn

---
+++ Public/Initialize-Script.ps1
@@ -0,0 +1,94 @@
+﻿function Initialize-Script {
+param (
+  [Parameter(Mandatory = $false)]
+  [string]
+  $Path = (Read-Host -prompt "Root path where script should be scaffolded (./): "),
+  [Parameter(Mandatory = $true, HelpMessage = "May only be made up of numbers, letters, and some special characters. Regex that passes: ^[a-zA-Z]{1}[\w\d_-]+[a-zA-Z0-9]{1}$")]
+  [string]
+  [ValidatePattern("^[a-zA-Z]*[\w\d_-]*[a-zA-Z0-9]*$")]
+  $ScriptName
+)
+function Invoke-Scaffold {
+  [CmdletBinding()]
+  param ()
+  process {
+    try {
+      if (($null -eq $Path) -or '' -eq $Path) {
+        $Path = "./"
+      }
+
+      $scriptFilePath = "$Path\$ScriptName.ps1"
+
+      New-Item "$scriptFilePath" -ItemType File
+
+      $mainFile = @"
+Set-StrictMode -Version 1
+
+`$startTime = Get-Date
+`$logDate = `$startTime.ToString("yyyy-MM-dd") 
+`$logFileName = `"$ScriptName`"
+`$logFile = "`$(`$logFileName)_`$(`$logDate)_log.txt"
+
+function Program {
+  return 0
+}
+
+function Invoke-$ScriptName {
+  [CmdletBinding()]
+  param ()
+  `$msg = "Starting process. `$(Get-Date)"
+  `$msg >> `$logFile
+  try {
+    Program -ErrorAction Stop
+  }
+
+  catch {
+    `$errorDetails = Get-ErrorDetails -error `$_
+    `$msg = "Top level issue:``n"
+    `$msg += `$errorDetails | ConvertTo-Json
+    `$msg >> `$logFile
+    throw `$_
+  }
+
+  finally {
+    `$msg = "Finished process. `$(Get-Date)``n"
+    `$msg >> `$logFile
+  }
+}
+
+function Get-ErrorDetails {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory=`$true)]
+    `$error
+  )
+
+  return @{
+    ScriptStackTrace = `$error.ScriptStackTrace
+    StackTrace = `$error.Exception.StackTrace
+    Message = `$error.Exception.Message
+    FullyQualifiedErrorId = `$error.FullyQualifiedErrorId
+    TargetObject = `$error.TargetObject
+    ErrorDetails = `$error.ErrorDetails
+  }
+}
+
+Invoke-$ScriptName -ErrorAction Stop
+
+"@
+
+      $mainFile > "$scriptFilePath"
+
+      $content = Get-Content -Path $scriptFilePath
+      Set-Content -Path $scriptFilePath -Value $content -Encoding UTF8 -PassThru -Force
+
+    }
+    catch {
+      Write-Error $_
+    }
+  }
+}
+Invoke-Scaffold -ErrorAction Stop
+}
+
+Initialize-Script
