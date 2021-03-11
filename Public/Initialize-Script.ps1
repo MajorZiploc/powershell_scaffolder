de9abee3c739be6d77da53f01e1eb924844bd9ff
Manyu Lakhotia <abhimanyu.lakhotia@acadian.com>
2021-03-11 08:55:55 -0600

    Uses shared res in init script fn

--- Public/Initialize-Script.ps1
+++ Public/Initialize-Script.ps1
@@ -1,4 +1,6 @@
-﻿function Initialize-Script {
+﻿. "$PSScriptRoot/../Private/Shared-Res.ps1"
+
+function Initialize-Script {
 param (
   [Parameter(Mandatory = $false)]
   [string]
@@ -26,6 +28,7 @@ function Invoke-Scaffold {
 
       New-Item "$scriptFilePath" -ItemType File
 
+      $errorHelper = Get-ErrorHelperContent
       $logFile = ""
       $logHelper = ""
       $logCleanupStep = ""
@@ -43,39 +46,7 @@ New-Item -ItemType Directory -Force -Path "`$logFolder" | Out-Null
     Clean-Logs -logFileNamePrefix `$logFileName -keepLogsForNDays `$keepLogsForNDays -logFolder "`$logFolder"
 "@
 
-      $logHelper = @"
-
-function Clean-Logs {
-  [CmdletBinding()]
-  param(
-    [Parameter(Mandatory = `$true)]
-    [string]
-    `$logFileNamePrefix
-    ,
-    [Parameter(Mandatory = `$true)]
-    [ValidateRange(0, [int]::MaxValue)]
-    [int]
-    `$keepLogsForNDays
-    ,
-    [Parameter(Mandatory = `$true)]
-    [string]
-    `$logFolder
-  )
-  [array]`$logs = Get-ChildItem -Path "`$logFolder/`$logFileName" | Where-Object {`$_.Name -imatch "`$(`$logFileNamePrefix)_(\S+)?_log\.txt"}
-  `$logs | ForEach-Object {
-    `$r = (`$_.Name | Select-String -Pattern "`$(`$logFileNamePrefix)_(\S+)?_log\.txt");
-    `$match = `$r.Matches.Groups[1].Value;
-    [datetime]`$logDate = `$match
-    `$now = Get-Date
-    `$timespan = `$now - `$logDate
-    `$daysOld = `$timespan.Days
-    if (`$daysOld -gt `$keepLogsForNDays) {
-      # delete the log file
-      Remove-Item -Path `$_.FullName
-    }
-  }
-}
-"@
+      $logHelper = Get-LogHelperContent
 
       }
       else {
@@ -120,22 +91,7 @@ function Invoke-$ScriptName {
   }
 }
 
-function Get-ErrorDetails {
-  [CmdletBinding()]
-  param (
-    [Parameter(Mandatory=`$true)]
-    `$error
-  )
-
-  return @{
-    ScriptStackTrace = `$error.ScriptStackTrace
-    StackTrace = `$error.Exception.StackTrace
-    Message = `$error.Exception.Message
-    FullyQualifiedErrorId = `$error.FullyQualifiedErrorId
-    TargetObject = `$error.TargetObject
-    ErrorDetails = `$error.ErrorDetails
-  }
-}
+$errorHelper
 
 $logHelper
 
